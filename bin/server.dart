6934021fcb5fc25301b7b9507745b51849644e99
ingego <1ingego1@gmail.com>
2023-02-15 13:14:47 +0300

    testing update

--- bin/server.dart
+++ bin/server.dart
@@ -1,21 +1,70 @@
 import 'dart:io';
 
-void main() async {
-  var server = await HttpServer.bind(InternetAddress.anyIPv6, 19133);
-  print('Proxy server listening on port ${server.port}');
-  await for (var request in server) {
-    // Обрабатывайте запросы здесь
-//     var targetUri = Uri.parse('https://www.google.com/');
+import 'package:logger/logger.dart';
 
-// var client = http.Client();
-// var targetRequest = http.Request(request.method, targetUri);
-// targetRequest.bodyBytes = (await request.toList());
+class Voxel {
+  double x;
+  double y;
+  double z;
+  double red;
+  double green;
+  double blue;
 
-// var targetResponse = await client.send(targetRequest);
-// response.statusCode = targetResponse.statusCode;
-// response.headers.addAll(targetResponse.headers);
+  Voxel(
+      {required this.x,
+      required this.y,
+      required this.z,
+      required this.red,
+      required this.green,
+      required this.blue});
+}
 
-// await targetResponse.stream.pipe(response);
+class PlyParser {
+  List<Voxel> voxels = [];
 
+  void parseFile(String fileName) {
+    var inputFile = File(fileName);
+    var lines = inputFile.readAsLinesSync();
+
+    bool inHeader = true;
+    double numVertices = 0;
+    double numFaces = 0;
+
+    for (String line in lines) {
+      if (inHeader) {
+        if (line.startsWith('element vertex')) {
+          numVertices = double.parse(line.split(' ')[2]);
+        } else if (line.startsWith('element face')) {
+          numFaces = double.parse(line.split(' ')[2]);
+        } else if (line.startsWith('end_header')) {
+          inHeader = false;
+        }
+      } else if (voxels.length < numVertices) {
+        var parts = line.split(' ');
+        var x = double.parse(parts[0]);
+        var y = double.parse(parts[1]);
+        var z = double.parse(parts[2]);
+        var red = double.parse(parts[3]);
+        var green = double.parse(parts[4]);
+        var blue = double.parse(parts[5]);
+
+        voxels.add(Voxel(x: x, y: y, z: z, red: red, green: green, blue: blue));
+      }
+    }
   }
+}
+
+void main() {
+  var plyParser = PlyParser();
+  plyParser.parseFile('untitled.ply');
+  var voxels = plyParser.voxels;
+  StackTrace trace = StackTrace.fromString("test");
+  print(trace);
+
+  Logger().d(voxels.map((e) {
+    if (e.blue == 255 && e.green == 255 && e.red == 255) {
+    } else {
+      return "${e.blue} ${e.green} ${e.red}";
+    }
+  }));
 }
